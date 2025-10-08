8af5c20f70c4b7eedf9f14aa79c3e3337ec86eaa
João Gabriel <jota.amaralmiranda@gmail.com>
2025-10-08 14:56:05 -0300

    Changing logger files: Print in terminal

--- worker/worker_lib/logger.py
+++ worker/worker_lib/logger.py
@@ -5,6 +5,7 @@ Configuração do logger global usando loguru.
 
 from loguru import logger
 from random import randint
+import sys
 
 # Nome do arquivo de log para o worker
 ARQUIVO_LOG = 'log_worker.txt'
@@ -16,8 +17,15 @@ logger.add(
     ARQUIVO_LOG,
     format="<green>{time:DD/MM/YYYY HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     level="INFO",
-    rotation="10 MB",  # Rotaciona o arquivo de log quando atinge 10 MB
-    catch=True         # Captura exceções inesperadas
+    rotation="10 MB",
+    catch=True
+)
+
+# Adiciona também sink para console (stdout) para aparecer no terminal
+logger.add(
+    sys.stdout, 
+    format="<green>{time:DD/MM/YYYY HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>", 
+    level="INFO"
 )
 
 logger = logger.bind(id=IDENTIFICACAO)
